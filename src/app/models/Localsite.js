ccbe06fe7b3f3c7ab642b58300c1a8ba8ed7e2e9
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2023-12-01 07:59:17 -0300

    Criado models LocalSite

---
+++ src/app/models/Localsite.js
@@ -0,0 +1,28 @@
+import Sequelize, { Model } from 'sequelize'
+
+class Localsite extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        nome: Sequelize.STRING,
+        cep: Sequelize.STRING,
+        estado: Sequelize.STRING,
+        cidade: Sequelize.STRING,
+        numero: Sequelize.INTEGER,
+        cnpj: Sequelize.STRING,
+        bairro: Sequelize.STRING,
+        rua: Sequelize.STRING,
+      },
+      {
+        sequelize,
+        modelName: 'Localsite',
+        name: {
+          singular: 'localsite',
+          plural: 'localsites',
+        },
+      }
+    )
+  }
+}
+
+export default Localsite
