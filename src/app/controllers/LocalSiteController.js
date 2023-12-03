189ec4fa9d8753ffc01abc4f0f68fd1e9d64f6b5
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2023-12-03 08:40:02 -0300

    finalizado rota para listar todos os sites cadastrado no sistema

--- src/app/controllers/LocalSiteController.js
+++ src/app/controllers/LocalSiteController.js
@@ -1,10 +1,134 @@
+import { Op } from 'sequelize'
+import { parseISO } from 'date-fns'
 import * as Yup from 'yup'
 import Localsite from '../models/Localsite'
 import { UniqueConstraintError } from 'sequelize'
 
 class LocalSiteController {
   async index(req, res) {
-    return res.json({ hello: 'Olá Local site' })
+    const { nome, cep, estado, cidade, numero, bairro, rua, createdBefore, createdAfter, updatedBefore, updatedAfter, sort } = req.query
+
+    const page = req.query.page || 1
+    const limit = req.query.limit || 25
+
+    let where = {}
+    let order = []
+
+    if (nome) {
+      where = {
+        ...where,
+        nome: {
+          [Op.iLike]: nome,
+        },
+      }
+    }
+
+    if (cep) {
+      where = {
+        ...where,
+        cep: {
+          [Op.like]: cep,
+        },
+      }
+    }
+
+    if (estado) {
+      where = {
+        ...where,
+        estado: {
+          [Op.iLike]: estado,
+        },
+      }
+    }
+
+    if (cidade) {
+      where = {
+        ...where,
+        cidade: {
+          [Op.iLike]: cidade,
+        },
+      }
+    }
+
+    if (numero) {
+      where = {
+        ...where,
+        numero: {
+          [Op.eq]: numero,
+        },
+      }
+    }
+
+    if (bairro) {
+      where = {
+        ...where,
+        bairro: {
+          [Op.iLike]: bairro,
+        },
+      }
+    }
+
+    if (rua) {
+      where = {
+        ...where,
+        rua: {
+          [Op.iLike]: rua,
+        },
+      }
+    }
+
+    if (createdBefore) {
+      where = {
+        ...where,
+        createdAt: {
+          [Op.gte]: parseISO(createdBefore), //lê uma String data e transforma em objeto data
+        },
+      }
+    }
+    if (createdAfter) {
+      where = {
+        ...where,
+        createdAt: {
+          [Op.lte]: parseISO(createdAfter), //lê uma String data e transforma em objeto data
+        },
+      }
+    }
+    if (updatedBefore) {
+      where = {
+        ...where,
+        updatedAt: {
+          [Op.gte]: parseISO(updatedBefore), //lê uma String data e transforma em objeto data
+        },
+      }
+    }
+    if (updatedAfter) {
+      where = {
+        ...where,
+        updatedAt: {
+          [Op.lte]: parseISO(updatedAfter), //lê uma String data e transforma em objeto data
+        },
+      }
+    }
+    //localhost:3000?sort=name,email
+    //localhost:3000?sort=id:desc,email
+    if (sort) {
+      order = sort.split(',').map((item) => item.split(':'))
+    }
+
+    try {
+      const data = await Localsite.findAll({
+        where,
+        order,
+        limit,
+        offset: limit * page - limit,
+      })
+      if (!Object.keys(data).length) {
+        return res.status(404).json({ error: 'Nada foi localizado!' })
+      }
+      return res.status(200).json(data)
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal server error.' })
+    }
   }
   async show(req, res) {}
   async create(req, res) {
